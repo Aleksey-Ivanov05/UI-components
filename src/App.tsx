eef03179c6a283ad87c6cfc6d94df7cbb810a631
Aleksey Ivanov <lepri4dw@gmail.com>
2022-11-13 16:42:54 +0600

    Сделал первый уровень
    Отчистил проект от ненужных файлов, добавил кнопку "x" в компонент Модал

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Modal from "./components/Modal/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const cancel = () => setShowModal(false);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container m-3">
+        <button className="btn btn-primary me-2" onClick={() => setShowModal(true)}>Show modal</button>
+        <Modal show={showModal} title="Some kind a modal title" onClose={cancel}>
+          <div className="modal-body">
+            This is a modal content
+          </div>
+        </Modal>
+      </div>
+    </>
+
   );
 }
 
